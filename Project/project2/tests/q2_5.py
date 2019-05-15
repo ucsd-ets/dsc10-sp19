29c38cbfacaebc63575c0d96c6bcf916c66c291f
Michelle Yang <zomgitsmichelle@gmail.com>
2019-05-14 21:05:09 -0700

    Added project 2

---
+++ Project/project2/tests/q2_5.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'Question 2_5',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Your answer should be a bool(True or False)!
+          >>> type(points_to_null) == bool
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ Project/project2/tests/q4_7.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'Question 4_7',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Your answer should be in [0.005,0.025,0.95, 0.975, 0.99, 0.995].
+          >>> prob_sufficient_funds in [0.005,0.025,0.95, 0.975, 0.99, 0.995]
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
